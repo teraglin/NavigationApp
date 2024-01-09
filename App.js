942132eb1b7614dac358ee5cf5008c596418c124
Aidan Kirvan <67266954+teraglin@users.noreply.github.com>
2024-01-09 12:03:30 +1100

    Task: native navigation stack

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
 import CategoriesScreen from "./screens/CategoriesScreen";
+import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
@@ -11,10 +12,14 @@ export default function App() {
     <>
       <StatusBar style="dark" />
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator initialRouteName="MealsCategories">
           <Stack.Screen
             name="MealsCategories"
             component={CategoriesScreen}
+          />
+          <Stack.Screen
+            name="MealsOverview"
+            component={MealsOverviewScreen}
           />
         </Stack.Navigator>
       </NavigationContainer>
